2b61156ddc105413a9c9330d40a6256026f769a2
mardanca <124599569+mardanca@users.noreply.github.com>
2023-05-24 13:06:11 -0700

    Link containers in js

--- HCSscriptfiles/membership.js
+++ HCSscriptfiles/membership.js
@@ -8,11 +8,10 @@ const small_services = [
     "Free CV Revamp"
 ]
 
-const membershipLinks = [ 
-    "",
-    "",
-    "",
-]
+const bronzeLink = "";
+const silverLink = "";
+const goldLink = "";
+const platLink = "";
 
 
 const selectBtn = document.querySelectorAll(".selectBtn");
@@ -21,6 +20,7 @@ const closeMember = document.querySelector(".closeMember");
 const memberContainerForSmall = document.querySelector(".memberInfo-container");
 const memberTitle = document.querySelector(".memberInfo-title");
 const memberServices = document.querySelector(".memberInfo-services");
+const payOnline = document.querySelector(".online");
 // console.log(subjectOfEmail.value)
 
 const checkMark = `<i class="fa-solid fa-check"></i> `;
@@ -29,6 +29,16 @@ selectBtn.forEach( btn => {
     btn.addEventListener("click", (e)=>{
         window.localStorage.setItem("checkout", true);
         checkOutContainer.classList.remove("no-show");
+
+        if (btn.parentNode.classList.contains("platinum")){
+            payOnline.href = platLink;
+        } else if (btn.parentNode.classList.contains("silver")){
+            payOnline.href = silverLink;
+        } else if (btn.parentNode.classList.contains("gold")){
+            payOnline.href = goldLink;
+        } else if (btn.parentNode.classList.contains("bronze")){
+            payOnline.href = bronzeLink;
+        };
 
         if( e.target.classList.contains("memberInfo-select") ){
             closeMember.click();
@@ -45,19 +55,21 @@ smallBtn.forEach( btn => {
             memberContainerForSmall.style.backgroundColor = `#cd8032`;
             memberServices.innerHTML = `<li>${checkMark}${small_services[0]}</li>
             <li>${checkMark}${small_services[1]}</li>`;
-
+            payOnline.href = bronzeLink;
         } else if ( btn.classList.contains("s")){
             memberContainerForSmall.style.backgroundColor = `#C0c0c0`;
             memberServices.innerHTML = `<li>${checkMark}${small_services[0]}</li>
             <li>${checkMark}${small_services[1]}</li>
             <li>${checkMark}${small_services[2]}</li>`;
+            payOnline.href = silverLink;
         } else if ( btn.classList.contains("g")){
             memberContainerForSmall.style.backgroundColor = `#d6b600`;
             memberServices.innerHTML = `<li>${checkMark}${small_services[0]}</li>
             <li>${checkMark}${small_services[1]}</li>
             <li>${checkMark}${small_services[2]}</li>
             <li>${checkMark}${small_services[3]}</li>
-            <li>${checkMark}${small_services[4]}</li>`;
+            <li>${checkMark}${small_services[4]}</li>`;            
+            payOnline.href = goldLink;
         } else if ( btn.classList.contains("p")){
             memberContainerForSmall.style.backgroundColor = `#48BEFF`;
             memberServices.innerHTML = `<li>${checkMark}${small_services[0]}</li>
@@ -67,6 +79,7 @@ smallBtn.forEach( btn => {
             <li>${checkMark}${small_services[4]}</li>
             <li>${checkMark}${small_services[5]}</li>
             <li>${checkMark}${small_services[6]}</li>`;
+            payOnline.href = platLink;
         }
 
         memberTitle.innerHTML = btn.childNodes[1].innerHTML;
